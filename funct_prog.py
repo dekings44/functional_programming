800fc3748d231a5a1cef2b70255c0425967d9d90
Kingsley Ihemere <dekings44@gmail.com>
2022-11-07 20:52:42 +0000

    map function

--- funct_prog.py
+++ funct_prog.py
@@ -4,4 +4,10 @@ def multipy_by2(li):
         new_list.append(item*2)
     return new_list
 
-print(multipy_by2(8,6,7))
+print(multipy_by2([8,6,7]))
+
+def square_item(item):
+    return item*item
+
+#MAP
+print(list(map(square_item, [4,5,6])))
