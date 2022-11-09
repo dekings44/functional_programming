e4dbf18abf46c5eac8e650650922e8993ae7c3f0
Kingsley Ihemere <dekings44@gmail.com>
2022-11-09 22:02:34 +0000

    replacing the pound sign

--- trans_data.py
+++ trans_data.py
@@ -2,5 +2,9 @@ import pandas as pd
 
 
 hotel_data = pd.read_csv('hotel_data1.csv')
-hotel_data['room_price'].str.split('£', expand=True)
+hotel_data['price'] = hotel_data['room_price'].str.replace('£', '')
+#
+hotel_data['price'].str.replace(',', '')
+hotel_data['price'].str.strip()
+hotel_data['price'] = hotel_data['price'].astype(int)
 print(hotel_data.head())
