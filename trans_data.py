c7fdcea337dfd0ef620d5488106ff4c0e6eae5ff
Kingsley Ihemere <dekings44@gmail.com>
2022-11-11 23:01:25 +0000

    using groupby to understand the rating description and the price

--- trans_data.py
+++ trans_data.py
@@ -2,13 +2,16 @@ import pandas as pd
 
 
 hotel_data = pd.read_csv('hotel_data1.csv')
-hotel_data['price'] = hotel_data['room_price'].str.replace('£', '')
+hotel_data['price (£)'] = hotel_data['room_price'].str.replace('£', '')
 #
-hotel_data['price'].str.replace(',', '', regex=True)
-hotel_data['price'].str.strip()
-hotel_data['price'] = hotel_data['price'].apply(pd.to_numeric,errors='coerce')
+hotel_data['price (£)'].str.replace(',', '', regex=True)
+hotel_data['price (£)'].str.strip()
+hotel_data['price (£)'] = hotel_data['price (£)'].apply(pd.to_numeric,errors='coerce')
 
 hotel_data['total_reviews'] = hotel_data['total_reviews'].str.replace(' reviews', '', regex=True)
 hotel_data['total_reviews'] = hotel_data['total_reviews'].str.replace(',', '', regex=True)
 hotel_data['total_reviews'] = hotel_data['total_reviews'].apply(pd.to_numeric,errors='coerce')
 print(hotel_data.head())
+
+result = hotel_data.groupby(['rating_desciption','price (£)'])['total_reviews'].aggregate(['mean', 'count'])
+print(result)
