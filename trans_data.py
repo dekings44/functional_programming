53fbe85d4597ddddabad548df755d8f7353f85fd
Kingsley Ihemere <dekings44@gmail.com>
2022-11-11 22:28:10 +0000

    successfully replaced all unwanted character and converted price column to number

--- trans_data.py
+++ trans_data.py
@@ -4,7 +4,7 @@ import pandas as pd
 hotel_data = pd.read_csv('hotel_data1.csv')
 hotel_data['price'] = hotel_data['room_price'].str.replace('£', '')
 #
-hotel_data['price'].str.replace(',', '')
+hotel_data['price'].str.replace(',', '', regex=True)
 hotel_data['price'].str.strip()
-hotel_data['price'] = hotel_data['price'].astype(int)
+hotel_data['price'] = hotel_data['price'].apply(pd.to_numeric,errors='coerce')
 print(hotel_data.head())
